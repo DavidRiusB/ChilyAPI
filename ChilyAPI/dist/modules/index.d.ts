42f02481ee5b7b54dd77ee5b585c5f6854be69ac
Jonathan <158221181+JonathanACarabetta@users.noreply.github.com>
2024-06-19 18:45:02 -0300

    CHANGE appModule added productModule

--- ChilyAPI/dist/modules/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { UserModule } from './user/user.module';
-export { AdminModule } from './admin/admin.module';
-export { SuperAdminModule } from './super-admin/super-admin.module';
-export { DeliveryModule } from './delivery/delivery.module';
-export { AuthModule } from './auth/auth.module';
