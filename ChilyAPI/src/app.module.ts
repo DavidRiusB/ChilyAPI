5fd75ba2702232077c88044adff9237a669f5bfe
R7Marl <lukaelcapo2017@gmail.com>
2024-06-19 14:24:57 -0300

    added config db to appmodule

--- ChilyAPI/src/app.module.ts
+++ ChilyAPI/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeOrmConfig from './config/database';
 import {
   AdminModule,
   DeliveryModule,
@@ -11,6 +13,13 @@ import {
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }), TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => config.get('typeorm')
+    }),
     UserModule,
     AdminModule,
     SuperAdminModule,
