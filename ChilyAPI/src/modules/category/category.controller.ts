5a9389918bb534dcffe3f0cc9aface5fb96cc5a1
Esaul Alfacan <esaulalfacan@gmail.com>
2024-06-21 13:15:57 -0600

    docs: :memo: Clasification modules

--- ChilyAPI/src/modules/category/category.controller.ts
+++ ChilyAPI/src/modules/category/category.controller.ts
@@ -1,33 +1,46 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { Category } from './category.entity';
 import { createCategoryDto } from '../dto/createCategory.dto';
+import { DocumentationApiTagsModule } from 'src/docs';
 
 @Controller('category')
+@DocumentationApiTagsModule.clasification('category')
 export class CategoryController {
-    constructor(private readonly categoryService: CategoryService){}
-    @Get()
-    getCategories(){
-        return this.categoryService.getCategories();
-    }
+  constructor(private readonly categoryService: CategoryService) {}
+  @Get()
+  getCategories() {
+    return this.categoryService.getCategories();
+  }
 
-    @Get(':id')
-    getCategoryById(@Param('id') id: number){
-        return this.categoryService.getCategoryById(id);
-    }
+  @Get(':id')
+  getCategoryById(@Param('id') id: number) {
+    return this.categoryService.getCategoryById(id);
+  }
 
-    @Post("create")
-    createCategory(@Body() createCategoryDto: createCategoryDto){
-        return this.categoryService.createCategory(createCategoryDto);
-    }
+  @Post('create')
+  createCategory(@Body() createCategoryDto: createCategoryDto) {
+    return this.categoryService.createCategory(createCategoryDto);
+  }
 
-    @Put('update/:id')
-    updateCategory(@Param('id') id: number, @Body() updateCategoryDto: createCategoryDto){
-        return this.categoryService.updateCategory(id, updateCategoryDto);
-    }
+  @Put('update/:id')
+  updateCategory(
+    @Param('id') id: number,
+    @Body() updateCategoryDto: createCategoryDto,
+  ) {
+    return this.categoryService.updateCategory(id, updateCategoryDto);
+  }
 
-    @Delete('delete/:id')
-    deleteCategory(@Param('id') id: number){
-        return this.categoryService.deleteCategory(id);
-    }
+  @Delete('delete/:id')
+  deleteCategory(@Param('id') id: number) {
+    return this.categoryService.deleteCategory(id);
+  }
 }
