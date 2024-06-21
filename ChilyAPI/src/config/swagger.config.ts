12464da3c1abdc17e7b5e4cabe99fa43e473eb76
Esaul Alfacan <esaulalfacan@gmail.com>
2024-06-21 08:42:22 -0600

    docs: :memo: swagger initial setup

---
+++ ChilyAPI/src/config/swagger.config.ts
@@ -0,0 +1,16 @@
+// swagger.config.ts
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwaggerDocumentation(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Donde Chily API')
+    .setDescription(
+      'API para la gestión del restaurante Donde Chily y sus franquicias',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
