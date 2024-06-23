07e463d0ae3095371d6ce356a2809459075bc1a3
Luka Mateo Ayala <61598384+R7Marl@users.noreply.github.com>
2024-06-23 16:47:39 -0300

    Update database.ts

--- ChilyAPI/src/config/database.ts
+++ ChilyAPI/src/config/database.ts
@@ -16,7 +16,7 @@ const config = {
   migrations: ["dist/migrations/*{.ts,.js}"],
   logging: false,
   synchronize: true,
-  //dropSchema: true,
+  dropSchema: true,
 };
 
 export default registerAs("typeorm", () => config);
