e4d0561fc1effd98fe5ebd85dbd60302c7c8c12a
Jonathan <158221181+JonathanACarabetta@users.noreply.github.com>
2024-06-25 16:46:11 -0300

    FIX Categories Name are in UpperCase and icon now can be nulleable

--- ChilyAPI/src/modules/category/dto/createCategory.dto.ts
+++ ChilyAPI/src/modules/category/dto/createCategory.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl, MaxLength } from 'class-validator';
 import { DocumentationCreateCategoryDto } from 'src/docs/';
 
 export class createCategoryDto {
@@ -11,5 +11,6 @@ export class createCategoryDto {
 
   @IsNotEmpty()
   @IsString()
+  @IsUrl()
   icon: string;
 }
