c79bde31076a68c2a63edeee45b8008b6f560b00
Esaul Alfacan <esaulalfacan@gmail.com>
2024-06-25 13:18:40 -0600

    docs: Modules definition

--- ChilyAPI/src/modules/addresses/addresses.controller.ts
+++ ChilyAPI/src/modules/addresses/addresses.controller.ts
@@ -1,32 +1,42 @@
-import { Body, Controller, Get, Post, Put, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { AddressesService } from './addresses.service';
 import { CreateAddressDto } from './dto/createAddress.dto';
 import { UpdateAddressDto } from './dto/updateAddres.dto';
+import { DocumentationApiTagsModule } from 'src/docs';
 
 @Controller('addresses')
+@DocumentationApiTagsModule.clasification('addresses')
 export class AddressesController {
-    constructor(private readonly addressService: AddressesService) {}
-    @Get('/test')
-    async test() {
-        return 'hello wlr';
-    }
-    @Get('/user')
-    @UseGuards(JwtAuthGuard)
-    async getUserAddresses(@Query('id') id: number) {
-        console.log(id)
-        return await this.addressService.getUserAddresses(id);
-    }
+  constructor(private readonly addressService: AddressesService) {}
+  @Get('/test')
+  async test() {
+    return 'hello wlr';
+  }
+  @Get('/user')
+  @UseGuards(JwtAuthGuard)
+  async getUserAddresses(@Query('id') id: number) {
+    console.log(id);
+    return await this.addressService.getUserAddresses(id);
+  }
 
-    @Post('/add')
-    @UseGuards(JwtAuthGuard)
-    async addNewAddress(@Body() addressCreate: CreateAddressDto) {
-        return await this.addressService.addNewAddress(addressCreate);
-    }
+  @Post('/add')
+  @UseGuards(JwtAuthGuard)
+  async addNewAddress(@Body() addressCreate: CreateAddressDto) {
+    return await this.addressService.addNewAddress(addressCreate);
+  }
 
-    @Put('/update')
-    @UseGuards(JwtAuthGuard)
-    async updateAddress(@Body() addressUpdate: UpdateAddressDto) {
-        return await this.addressService.updateAddress(addressUpdate);
-    }
+  @Put('/update')
+  @UseGuards(JwtAuthGuard)
+  async updateAddress(@Body() addressUpdate: UpdateAddressDto) {
+    return await this.addressService.updateAddress(addressUpdate);
+  }
 }
--- ChilyAPI/src/modules/user/user.controller.ts
+++ ChilyAPI/src/modules/user/user.controller.ts
@@ -6,35 +6,37 @@ import {
   Param,
   Put,
   Query,
-} from "@nestjs/common";
-import { UserService } from "./user.service";
-import { UserDTO } from "../auth/dto/user.dto";
-import { UserUpdateDto } from "./user-update.dto";
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDTO } from '../auth/dto/user.dto';
+import { UserUpdateDto } from './user-update.dto';
+import { DocumentationApiTagsModule } from 'src/docs';
 
-@Controller("user")
+@Controller('user')
+@DocumentationApiTagsModule.clasification('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
   async getAllUsers(
-    @Query("page") page: number = 1,
-    @Query("limit") limit: number = 10
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
   ) {
     return await this.userService.findAll({ page, limit });
   }
 
-  @Get(":id")
-  async getUserbyId(@Param("id") id: number) {
+  @Get(':id')
+  async getUserbyId(@Param('id') id: number) {
     return await this.userService.findUserById(id);
   }
 
-  @Put(":id")
-  async updateUser(@Param("id") id: number, @Body() userData: UserUpdateDto) {
+  @Put(':id')
+  async updateUser(@Param('id') id: number, @Body() userData: UserUpdateDto) {
     return await this.userService.update(userData, id);
   }
 
-  @Delete(":id")
-  async softDeleteUser(@Param("id") id: number) {
+  @Delete(':id')
+  async softDeleteUser(@Param('id') id: number) {
     const user = await this.userService.softDelete(id);
     return user;
   }
