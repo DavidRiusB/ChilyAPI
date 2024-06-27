4a02b730137d3cee05b6638cda3a62e2dd42e8f1
David rius <davidriusbazurdo@gmail.com>
2024-06-27 09:58:27 -0500

    upload service

--- ChilyAPI/src/modules/upload/upload.service.ts
+++ ChilyAPI/src/modules/upload/upload.service.ts
@@ -1,25 +1,25 @@
-import {
-  PutObjectAclCommand,
-  PutObjectCommand,
-  S3Client,
-} from "@aws-sdk/client-s3";
 import { Injectable } from "@nestjs/common";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class UploadService {
-  private readonly s3Client = new S3Client({
-    region: this.configService.getOrThrow("AWS_S3_REGION"),
-  });
-  constructor(private readonly configService: ConfigService) {}
+  private readonly s3Client: S3Client;
 
-  async upload(fileName: string, file: Buffer) {
-    await this.s3Client.send(
+  constructor(private readonly configService: ConfigService) {
+    this.s3Client = new S3Client({
+      region: this.configService.getOrThrow("AWS_S3_REGION"),
+    });
+  }
+
+  async upload(bucket: string, fileName: string, file: Buffer) {
+    const upload = await this.s3Client.send(
       new PutObjectCommand({
-        Bucket: "buckerName",
+        Bucket: bucket,
         Key: fileName,
         Body: file,
       })
     );
+    return upload;
   }
 }
