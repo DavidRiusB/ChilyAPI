7eb6dd6df2ba9eef82a56dc7c2ecb256a5f820b3
Jonathan <158221181+JonathanACarabetta@users.noreply.github.com>
2024-06-27 21:27:03 -0300

    CHANGE interceptor QueryFilterInterceptor

--- ChilyAPI/src/common/interceptors/queryFilter.interceptor.ts
+++ ChilyAPI/src/common/interceptors/queryFilter.interceptor.ts
@@ -9,6 +9,10 @@ export class QueryFilterInterceptor implements NestInterceptor {
         const limit = request.query.limit ? parseInt(request.query.limit) : 5;
         const min = request.query.min ? parseInt(request.query.min) : 0;
         const max = request.query.max ? parseInt(request.query.max) : Infinity;
+        const search = request.query.search? request.query.search : '';
+        const filter = request.query.filter? request.query.filter : '';
+        request.search = search;
+        request.filter = filter;
         request.page = page;
         request.limit = limit;
         request.min = min;
