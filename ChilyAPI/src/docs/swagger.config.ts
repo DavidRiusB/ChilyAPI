01334f6db47baffcf3a6c31d9a17bed7e3a0fc4b
Esaul Alfacan <esaulalfacan@gmail.com>
2024-07-02 23:22:01 -0600

    docs: :memo: Update Documentation
    Change in swagger module, dto envs

--- ChilyAPI/src/docs/swagger.config.ts
+++ ChilyAPI/src/docs/swagger.config.ts
@@ -1,31 +1,20 @@
-// swagger.config.ts
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as fs from "fs-extra";
+import { EnvironmentVariablesDto } from "./docs-enviroments.dto";
 
 export function DocumentationConfig(app: INestApplication): void {
   const config = new DocumentBuilder()
     .setTitle("Donde Chily API")
     .setDescription(
-      "API para la gestión del restaurante Donde Chily y sus franquicias",
+      "API para la gestión del restaurante Donde Chily y sus franquicias. Consulta las variables de entorno en el archivo README.md para más detalles.",
     )
     .setVersion("1.0")
-    .addCookieAuth("Token en Cookies", { type: "apiKey", in: "cookie" })
-    .addOAuth2({
-      type: "oauth2",
-      flows: {
-        authorizationCode: {
-          authorizationUrl: process.env.BACKEND_URL + "/auth/google/login",
-          tokenUrl: process.env.BACKEND_URL + "/auth/google/redirect",
-          scopes: {
-            "https://www.googleapis.com/auth/userinfo.profile":
-              "Read user profile information",
-            "https://www.googleapis.com/auth/userinfo.email": "Read user email",
-          },
-        },
-      },
-    })
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    extraModels: [EnvironmentVariablesDto],
+  });
+
   SwaggerModule.setup("api", app, document);
 }
