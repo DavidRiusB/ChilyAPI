e7a8d430f9426032445831e2e53fd5b8700c5faf
R7Marl <lukaelcapo2017@gmail.com>
2024-07-11 02:30:21 -0300

    feat: helper pdfkit function, add route in order controller to generate invoice

--- ChilyAPI/src/config/database.ts
+++ ChilyAPI/src/config/database.ts
@@ -17,7 +17,7 @@ const config = {
   migrations: ["dist/migrations/*{.ts,.js}"],
   logging: false,
   synchronize: true,
-  dropSchema: true,
+  dropSchema: false,
 };
 
 export default registerAs("typeorm", () => config);
