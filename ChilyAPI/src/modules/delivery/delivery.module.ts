1012389b5f0b0bce7da4690d12f6c11e8048939d
R7Marl <lukaelcapo2017@gmail.com>
2024-07-12 02:43:32 -0300

    del: deleted unused modules

--- ChilyAPI/src/modules/delivery/delivery.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DeliveryController } from './delivery.controller';
-import { DeliveryService } from './delivery.service';
-
-@Module({
-  controllers: [DeliveryController],
-  providers: [DeliveryService]
-})
-export class DeliveryModule {}
