c6f71a6e22d5400ba45808e28386ce7ee27e2494
R7Marl <lukaelcapo2017@gmail.com>
2024-07-12 02:39:51 -0300

    feat: Protected routes for products, discount, and delete order-details controller

--- ChilyAPI/src/modules/discount/discount.controller.ts
+++ ChilyAPI/src/modules/discount/discount.controller.ts
@@ -7,10 +7,15 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
 } from "@nestjs/common";
 import { DiscountService } from "./discount.service";
 import { createDiscountDto, updateDiscountDto } from "./dto/createDiscount.dto";
 import { DocumentationApiTagsModule } from "src/docs";
+import { JwtAuthGuard } from "../auth/guards/jwt.guard";
+import { RolesGuard } from "src/common/guards/roles.guard";
+import { Roles } from "src/common/decorators/roles.decorator";
+import { Role } from "src/common/enums";
 
 @Controller("discount")
 @DocumentationApiTagsModule.clasification("Rutas para: Descuentos")
@@ -38,26 +43,36 @@ export class DiscountController {
   }
 
   @Post("create")
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin || Role.SuperAdmin)
   creatediscount(@Body() createDiscount: createDiscountDto) {
     return this.discountService.creatediscount(createDiscount);
   }
 
   @Put("update/:id")
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin || Role.SuperAdmin)
   updateDiscount(@Param("id") id: number, @Body() updateDiscount: updateDiscountDto) {
     return this.discountService.updateDiscount(id, updateDiscount);
   }
 
   @Put("invalid")
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin || Role.SuperAdmin)
   InvalidDiscount(@Query("code") code: string, @Query("userId") userId: string) {
     return this.discountService.InvalidDiscount(code, userId);
   }
 
   @Put("setdiscount")
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin || Role.SuperAdmin)
   setDiscountToUser(@Query("discount") discount: string, @Query("userId") userId: string) {
     return this.discountService.setDiscountToUser(discount, userId);
   }
 
   @Delete("delete/:id")
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin || Role.SuperAdmin)
   deleteDiscount(@Param("id") id: number) {
     return this.discountService.deleteDiscount(id);
   }
