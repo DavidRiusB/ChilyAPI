31ce76e6ae5743d7e0ff7013b39e0204fc339885
Esaul Alfacan <esaulalfacan@gmail.com>
2024-07-13 15:44:21 -0600

    docs: :memo: Create documentation to upload module

--- ChilyAPI/src/modules/upload/upload.controller.ts
+++ ChilyAPI/src/modules/upload/upload.controller.ts
@@ -2,24 +2,26 @@ import {
   Controller,
   Post,
   UploadedFile,
-  UseInterceptors,
+  UseInterceptors
 } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { UploadService } from "./upload.service";
+import { DocumentationApiTagsModule } from "src/docs";
+import { DocumentationUploadImg } from "src/docs/doc-upload-module/docs-endpoint-upload";
 
 @Controller("upload")
+@DocumentationApiTagsModule.clasification("Rutas para: Cargar archivos")
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
   //For testing
-   @Post()
+  @Post()
+  @DocumentationUploadImg()
   @UseInterceptors(FileInterceptor("image"))
   async uploadImg(@UploadedFile() img: Express.Multer.File) {
     console.log(img);
 
-    const upload = await this.uploadService.update(
-      img
-    );
+    const upload = await this.uploadService.update(img);
     console.log(upload.secure_url);
-    return {secure_url: upload.secure_url}
+    return { secure_url: upload.secure_url };
   }
 }
